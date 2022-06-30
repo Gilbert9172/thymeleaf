041f7b7dabbb2c2c6f5912da0f42bdaf7b6a0aaa
gilbert9172 <gilbert9172@naver.com>
2022-06-30 19:56:19 +0900

    validation

---
+++ src/main/java/hello/itemservice/web/validation/ValidationItemApiController.java
@@ -0,0 +1,38 @@
+package hello.itemservice.web.validation;
+
+import hello.itemservice.web.validation.form.ItemSaveForm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/validation/api/items")
+public class ValidationItemApiController {
+
+    @PostMapping("/add")
+    public Object addItem(@RequestBody @Validated ItemSaveForm form, BindingResult bindingResult) {
+
+        log.info("API 컨츠롤러 호출");
+
+        // 최소 주문 금액 10000원 이상.
+        if (form.getPrice() != null && form.getQuantity() != null) {
+            int resultPrice = form.getPrice() * form.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000,1000000}, null);
+            }
+        }
+        
+        if (bindingResult.hasErrors()) {
+            log.info("검증 오류 발생 errors ={}", bindingResult);
+            return bindingResult.getAllErrors();
+        }
+
+        log.info("성공 로직 실행");
+        return form;
+    }
+}
